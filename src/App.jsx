19da3e4e0dffc8ad4f8307e20178b0ddd34dd465
brickphone <mattis.hutt@gmail.com>
2023-06-08 13:17:38 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Hero from './components/hero'
 import Cards from './components/cards'
 import katie from './public/images/katie-zaferes.png'
 import wedding from './public/images/wedding-photography.png'
+import bike from './public/images/mountain-biking.png'
 
 function App () {
   return (
@@ -10,6 +11,7 @@ function App () {
       <Navbar />
       <Hero />
       <Cards 
+        id="1"
         img={katie}
         rating="5.0"
         amount="(6)"
@@ -17,13 +19,22 @@ function App () {
         description="Life lessons with Katie Zafares"
         price="From $136 "
       />
-      <Cards 
+      <Cards
+        id="2" 
         img={wedding}
         rating="5.0"
         amount="(30)"
         country="USA"
         description="Learn wedding photography"
         price="From $125"
+      />
+      <Cards 
+        id="3"
+        img={bike}
+        rating="4.8"
+        amount="(2)"
+        description="Group Mountain Biking"
+        price="From $50"
       />
     </div>
   )
