d8a86e3914a8165b3f90e319ceda5693279c5d02
brickphone <mattis.hutt@gmail.com>
2023-06-08 22:23:44 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,18 @@
 import Navbar from './components/navbar'
 import Hero from './components/hero'
 import Cards from './components/cards'
-import katie from './public/images/katie-zaferes.png'
-import wedding from './public/images/wedding-photography.png'
-import bike from './public/images/mountain-biking.png'
+import data from './components/data'
 
 function App () {
+  const Cards = data.map(item => {
+
+  })
+
   return (
     <div className='main-container'>
       <Navbar />
       <Hero />
-      <Cards 
-        id="1"
-        img={katie}
-        rating="5.0"
-        amount="(6)"
-        country="USA"
-        description="Life lessons with Katie Zafares"
-        price="From $136 "
-      />
-      <Cards
-        id="2" 
-        img={wedding}
-        rating="5.0"
-        amount="(30)"
-        country="USA"
-        description="Learn wedding photography"
-        price="From $125"
-      />
-      <Cards 
-        id="3"
-        img={bike}
-        rating="4.8"
-        amount="(2)"
-        description="Group Mountain Biking"
-        price="From $50"
-      />
+      {Cards}
     </div>
   )
 }
---
+++ src/components/data.js
@@ -0,0 +1,32 @@
+import katie from './public/images/katie-zaferes.png'
+import wedding from './public/images/wedding-photography.png'
+import bike from './public/images/mountain-biking.png'
+
+export default [
+  {
+    id:1,
+    img:{katie},
+    rating:5.0,
+    amount:6,
+    country:"USA",
+    description: "Life lessons with Katie Zafare",
+    price: 136,
+  },
+  {
+    id:2,
+    img:{wedding},
+    rating:5.0,
+    amount:30,
+    country:"USA",
+    description: "Learn wedding photography",
+    price:125
+  },
+  {
+    id:3,
+    img:{bike},
+    rating:4.8,
+    amount:2,
+    description:"Group Mountain Biking",
+    price:50,
+  }
+]
